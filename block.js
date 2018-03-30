9ae08b990d49b57ab9b1f1dddf3c93c1bd068ba3
Nelson Campos <ncamposfh@gmail.com>
2018-03-29 21:34:11 -0300

    primeiro block

---
+++ block.js
@@ -0,0 +1,19 @@
+class Block {
+    constructor(timestamp, lasthash, hash, data) {
+        this.timestamp = timestamp;
+        this.lasthash = lasthash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString(){
+        return `Block - 
+            Timestamp: ${this.timestamp}
+            Lasthash : ${this.lasthash.substring(0,10)}
+            Hash     : ${this.hash.substring(0,10)}
+            Data     : ${this.data}
+        `
+    }
+}
+
+module.exports = Block;
